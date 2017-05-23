cefdc8434b44014a1b064fbe4fca63aae61e8dbc
ealbek <ealbek-contractor@shaklee.com>
2017-05-23 11:34:40 -0700

    Added RequestBody annotation

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -4,22 +4,12 @@ package hello;
  * Data object sent in requests and responses.
  */
 public class Greeting {
-	public long id; 
+
+	public long id;
 	public String content;
+	public Child c1;
 
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
+	public static class Child {
+		public String fieldA;
 	}
 }
--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -3,6 +3,7 @@ package hello;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class GreetingController {
 
 	@RequestMapping(path = "/greeting", method = POST)
-	public Greeting save(Greeting g) {
-		return g;
+	public Greeting save(Greeting greet) {
+		return greet;
+	}
+
+	@RequestMapping(path = "/greetingWithRequestBody", method = POST)
+	public Greeting withRequestBody(@RequestBody Greeting greet) {
+		return greet;
 	}
 
 	@RequestMapping(path = "/greetingWithModelAttribute", method = POST)
-	public Greeting withModelAttribute(@ModelAttribute Greeting g) {
-		return g; 
+	public Greeting withModelAttribute(@ModelAttribute Greeting greet) {
+		return greet;
 	}
 }
