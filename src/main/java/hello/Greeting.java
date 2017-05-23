1b5dc7e5270717bd93cb9d68ad8224f996bce399
ealbek <ealbek-contractor@shaklee.com>
2017-05-22 17:31:58 -0700

    Added getters and setters, only results in different problems

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -6,4 +6,20 @@ package hello;
 public class Greeting {
 	public long id; 
 	public String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
